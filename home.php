a2bcbb5948dfcb62e0f99ab1e9d898c7a88a73af
Fikriyah <nurfikriyahrosman@gmail.com>
2021-02-15 23:46:40 +0800

    gambar

--- home.php
+++ home.php
@@ -2,5 +2,32 @@
 include 'header.html';
 ?>
 <body>
-<p>Hi</p>
+<style>
+  .rewardimg {
+  object-fit: contain;
+  display: block; 
+  overflow: auto; 
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
+<div class="col-sm-4 col-lg-3 col-md-3">
+                          
+                          <div class="card" style="border:1px solid #ccc; border-radius:5px; padding:16px; margin-bottom:16px; ">
+                              <div class="d-flex card-img-top"><img src="img/jaehyun.jpg" width="180" height="200"  alt="Reward Picture" class="rewardimg"></div>
+   
+                              <div class="card-body">
+                                <h5 class="card-title">
+                                  <span class='badge badge-success'>
+                                    Artist
+                                  </span>
+                                  <strong>NCT Jaehyun</strong>
+                                </h5>
+                                <p class="card-text">
+                                  Date: 15/02/2021
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
 </body>
